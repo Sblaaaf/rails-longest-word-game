deacaa8a0622206defdb72cbfa61a0d43afdf039
renaud lourgouilloux lourgouilloux.renaud@gmail.com <lourgouilloux.renaud@gmail.com>
2024-05-20 18:29:43 +0200

    Rails - 03 LongestWord - last push that's work

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,16 +1,69 @@
+require "json"
+require "open-uri"
+require "uri"
+
 class GamesController < ApplicationController
   def new
-    # @letters = ("A".."Z").to_a.sample(10)
     voyelles = %w[A E I O U]
-    # consonnes = ('A'..'Z').to_a - voyelles
     @letters = voyelles.sample(3) + ("A".."Z").to_a.sample(7) # + consonnes.sample(7)
     @letters.shuffle!
+
+    # @letters = ("A".."Z").to_a.sample(10)
+
+    # consonnes = ('A'..'Z').to_a - voyelles
     # @letters += Array.new(7) { (('A'..'Z').to_a - voyelles).sample }
     # @letters.shuffle!
   end
+
+  def shuffle
+    @letters.shuffle!
+  end
+
   def score
     # raise
-    @word = params[:word]
-    @letters = params[:letters]
+    # @letters = params[:letters]
+    # @word = params[:word]
+
+    # récupérer les lettres générées
+    @letters = params[:letters].split
+    # récupérer les lettres écrites par le USER
+    @word = (params[:word] || "").upcase
+    # comparer les lettres
+    @compare = compare?(@word, @letters)
+    # mot max-length = 10
+    # vérifier si le mot est valide avec l'API
+    @url = "https://dictionary.lewagon.com/#{:word}"
+    @valid = valid?(@word)
+    # message valide
+    # if !valid?
+    #   @message = "No match !"
+    # # ou message invalide
+    # elsif !valid?
+    #   @message = "Invalid word ! 👎"
+    # else
+    #   @message = "Well done ! 👍"
+    # end
+
+    # afficher le mot choisit
+    # @yourWord = params[:word]
+    # calculer le score
+    # @score
+    # calculer le temps de réponse
+    # @time
+    # recommencer avec les mêmes lettres de base
+    # ou new game
+  end
+
+  private
+
+  def valid?(word)
+    url = URI("https://dictionary.lewagon.com/#{word}")
+    response = URI.open(url).read
+    json = JSON.parse(response)
+    json['found']
+  end
+
+  def compare?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
   end
 end
